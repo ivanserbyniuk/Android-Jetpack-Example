93eb2cbe2174f05af6fc60dc84f9ef6bd27e7079
ivanserbyniuk <ivan.serbyniuk@gmail.com>
2018-07-20 18:23:20 +0300

    Remove logs

--- app/src/main/java/com/example/ivanserbyniuk/mvvmarchitectureexample/viewmodels/SimpleContViewModel.kt
+++ app/src/main/java/com/example/ivanserbyniuk/mvvmarchitectureexample/viewmodels/SimpleContViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.ivanserbyniuk.mvvmarchitectureexample.viewmodels
 
-import android.util.Log
 import com.example.ivanserbyniuk.mvvmarchitectureexample.network.models.Coin
 import com.example.ivanserbyniuk.mvvmarchitectureexample.network.repositories.CoinsRepositoryNetwork
 
@@ -9,8 +8,6 @@ class SimpleContViewModel : SimpleNetworkViewModel<List<Coin>?>() {
     override fun makeRequest() {
         CoinsRepositoryNetwork().getCoins()
                 .compose(progressTransformer())
-                .doOnSuccess { it -> Log.d("myLogs", " size " + it.size) }
                 .subscribe(defaultConsumer)
     }
-
 }
